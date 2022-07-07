bfb4cbd1aa7854173bb40667281b7d2db7560676
Francisco Krall <kriegger@protonmail.com>
2022-07-07 00:07:41 -0300

    Add files via upload

---
+++ getprices.py
@@ -0,0 +1,11 @@
+import requests
+from bs4 import BeautifulSoup
+import lxml
+def getprice(ticker, exchange):
+    url = 'https://www.google.com/finance/quote/'+ticker+':'+exchange
+    r = requests.get(url)
+    soup = BeautifulSoup (r.content, 'lxml')
+    price = str((soup.find(class_="YMlKec fxKbKc")))
+    return (float((price.replace('<div class="YMlKec fxKbKc">$','').replace('</div>',''))))
+#formato:getprice('ticker','exchange')
+print (getprice('TSLA','NASDAQ'))
